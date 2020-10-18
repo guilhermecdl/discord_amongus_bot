ff01dbc28678302a9ef06e80aa780edbdf9722c9
unknown <guilhermecdelucca@gmail.com>
2020-10-18 14:16:13 -0300

    add time

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import config
 
+horario = [] #o discord nao tava salvando a string, entao vai ficar em uma lista
 client = discord.Client()
 in_list = []
 
@@ -23,7 +24,10 @@ async def on_message(message):
         if auth not in in_list:
             if len(in_list) < 10:
                 in_list.append(auth)
-                msg = 'Amongãs hoje: '
+                if len(horario) > 0:
+                    ho = horario[-1]
+                else: ho = ':'
+                msg = 'Amongãs hoje'+ho
                 for i in in_list:
                     msg = msg+'\n'+i
                 msg = msg.format(message)
@@ -34,16 +38,71 @@ async def on_message(message):
             else:
                 msg = f'Sinto muito {message.author.name}, já tem 10, fica esperto ai caso alguem desista.'.format(message)
                 await message.channel.send(msg)
-    
+
     elif message.content.startswith('out!'):
         auth = str(message.author.name)
         if auth in in_list:
             in_list.remove(auth)
-            msg = 'Amongãs hoje: '
+            if len(horario) > 0:
+                ho = horario[-1]
+            else: ho = ':'
+            msg = 'Amongãs hoje'+ho
             for i in in_list:
                 msg = msg+'\n'+i
             msg = msg.format(message)
             await message.channel.send(msg)
+
+    elif message.content.startswith('!among us'):
+        cont = message.content
+        cont = cont.split(' ')
+        if len(cont) == 3:
+            hora = cont[2]
+            if 'h' not in hora:
+                msg = f'{message.author.mention} só entendo horario no formato XXh ou XXhXX'
+                msg = msg.format(message)
+                await message.channel.send(msg)
+                return    
+            elif len(hora) == 3:
+                h = hora[:-1]
+                m = 0
+            elif len(hora) == 5:
+                h = hora[:2]
+                m = hora[-2:]
+            else:
+                msg = f'{message.author.mention} só entendo horario no formato XXh ou XXhXX'
+                msg = msg.format(message)
+                await message.channel.send(msg)
+                return                
+            print(h) #! for testing
+            try:
+                h = int(h)
+                m = int(m)
+                if h > 24 or h < 0:
+                    msg = f'O dia só tem 24h {message.author.mention}...'
+                    msg = msg.format(message)
+                    await message.channel.send(msg)
+                    return
+                if m > 59 or m < 0:
+                    msg = f'Uma hora só tem 60 minutos {message.author.mention}...'
+                    msg = msg.format(message)
+                    await message.channel.send(msg)
+                    return       
+
+                horario.append(' '+hora+':')
+                msg = f'Marcado pras '+hora
+                msg = msg.format(message)
+                await message.channel.send(msg)
+
+
+            except ValueError:
+                msg = f'{message.author.mention} só entendo horario no formato XXh ou XXhXX'
+                msg = msg.format(message)
+                await message.channel.send(msg)
+                return
+
+
+
+            
 
 @client.event
 async def on_ready():
